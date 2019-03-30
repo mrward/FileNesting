2886481a15f5e9c9a6455a42c9ce58fb9c97ab04
Matt Ward <ward.matt@gmail.com>
2019-03-30 11:22:05 +0000

    Support MonoDevelop 8.0

--- src/MonoDevelop/AddinInfo.cs
+++ src/MonoDevelop/AddinInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly:Addin ("FileNesting",
 	Namespace = "MonoDevelop",
-	Version = "0.1",
+	Version = "0.1.1",
 	Category = "IDE extensions")]
 
 [assembly:AddinName ("FileNesting")]
 [assembly:AddinDescription ("Adds File Nesting support.")]
 
-[assembly:AddinDependency ("Core", "7.0")]
-[assembly:AddinDependency ("Ide", "7.0")]
+[assembly:AddinDependency ("Core", "8.0")]
+[assembly:AddinDependency ("Ide", "8.0")]
